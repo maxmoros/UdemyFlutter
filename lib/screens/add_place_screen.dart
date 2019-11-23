16b9902b12c97c968e44854bc507a4575608f55c
Max Moros <mmoros@e1z4r9p8.42.us.org>
2019-11-23 10:34:30 -0800

    lololol

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -54,6 +55,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(height: 10,),
+                    LocationInput(),
                   ],
                 ),
               ),
